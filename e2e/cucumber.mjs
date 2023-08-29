b1e2629c1210c55d36b4a10179307d17e1be2b0f
NomaLove <nomandladube2@gmail.com>
2023-08-29 19:54:53 +0200

    edited some config files

--- e2e/cucumber.mjs
+++ e2e/cucumber.mjs
@@ -1,9 +1,10 @@
 import dotenv from "dotenv"
+
 dotenv.config()
 const {env} = process
 const QUIET = env.QUIET ==="1"
-const PATHS = env.PATHS || "./FEATURE/**/*,{feature,feature,md}";
-const REQUIRE = env.REQUIRE || "./features/support/*.{ts,js}";
+const PATHS = env.PATHS || "./features/**/*,{feature,feature,md}";
+const REQUIRE = env.REQUIRE || "./step-definitions/*.{ts,js}";
 export default {
     parallel: 2,
     format: ['html:cucumber-report.html'],
