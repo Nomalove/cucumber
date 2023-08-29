7624b58a147b16875dbd27ee342b307e2c00d77a
NomaLove <nomandladube2@gmail.com>
2023-08-29 20:03:33 +0200

    fixing configt files

--- e2e/cucumber.mjs
+++ e2e/cucumber.mjs
@@ -1,15 +1,15 @@
-import dotenv from "dotenv"
+ import dotenv from "dotenv"
 
 dotenv.config()
 const {env} = process
 const QUIET = env.QUIET ==="1"
-const PATHS = env.PATHS || "./features/**/*,{feature,feature,md}";
-const REQUIRE = env.REQUIRE || "./step-definitions/*.{ts,js}";
-export default {
-    parallel: 2,
-    format: ['html:cucumber-report.html'],
-    publishQuiet : QUIET,
-    paths: [PATHS],
-    require :[REQUIRE],
-    requireModule:["ts-node/register"],
-  }
+ const PATHS = env.PATHS || "./features/**/*,{feature,feature,md}";
+ const REQUIRE = env.REQUIRE || "./step-definitions/*.{ts,js}";
+ export default {
+     parallel: 2,
+     format: ['html:cucumber-report.html'],
+     publishQuiet : QUIET,
+     paths: [PATHS],
+     require :[REQUIRE],
+     requireModule:["ts-node/register"],
+   }
