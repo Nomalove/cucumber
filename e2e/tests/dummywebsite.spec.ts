475406d0fb7768f134708f1bc52ff93ef4465896
NomaLove <nomandladube2@gmail.com>
2023-08-30 15:20:25 +0200

    added dummywebsite.spec.ts and index.html

--- e2e/tests/dummywebsite.spec.ts
+++ e2e/tests/dummywebsite.spec.ts
@@ -1,5 +1,8 @@
 import {expect,test} from '@playwright/test'
 
 test('has title', async ({page})=>{
-   await page.goto('')
-})
+   await page.goto('http://127.0.0.1:5500/cucumber/test-website/index.html');
+
+   await expect(page).toHaveTitle("Nomandla|Blog");
+});
+
